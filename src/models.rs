a343ce0c9e0ec9539bed91feae03321527977dbe
iammasterbrucewayne <batman@dotcodeschool.com>
2024-08-19 15:11:42 +0530

    add mongodb models

---
+++ src/models.rs
@@ -0,0 +1,39 @@
+use serde::{Deserialize, Serialize};
+
+/// A repository document. This is used to store information about the owner of the repository, the template used to create the repository, and the relationships between the repository and other documents.
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Repository {
+    pub name: String,
+    pub template: String,
+    pub relationships: Vec<Relationship>,
+}
+
+/// A user document. This is used to store information about the user, the repositories they own, and the relationships between the user and other documents.
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct User {
+    pub name: String,
+    pub repositories: Vec<Relationship>,
+    pub relationships: Vec<Relationship>,
+}
+
+/// A course document. This is used to store information about the course, the users enrolled in the course, and the relationships between the course and other documents.
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Course {
+    pub name: String,
+    pub relationships: Vec<Relationship>,
+}
+
+/// The type of document. This is used to identify the type of document in the relationships between documents.
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum DocumentType {
+    Repository,
+    User,
+    Course,
+}
+
+/// A relationship between documents. This is used to store the ID of the document and the type of document in the relationship.
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Relationship {
+    pub id: String,
+    pub r#type: DocumentType,
+}
