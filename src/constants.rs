f2cef29ea7c234226f34e5953851e2f8565afa48
iammasterbrucewayne <batman@dotcodeschool.com>
2024-08-22 14:15:28 +0530

    add const: SUBMISSION_COLLECTION

--- src/constants.rs
+++ src/constants.rs
@@ -4,3 +4,5 @@ pub(crate) const GIT_SERVER_URL: &str = "https://git.dotcodeschool.com";
 pub(crate) const DB_NAME: &str = "dcs-test";
 /// The name of the collection that stores the repository documents
 pub(super) const REPO_COLLECTION: &str = "repositories";
+/// The name of the collection that stores the submission documents
+pub(super) const SUBMISSION_COLLECTION: &str = "submissions";
